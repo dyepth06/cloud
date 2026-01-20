5bd4b75925e1d22b92c920f099e3ed1d7149c870
dyepth06 <jeffdelapena06@gmail.com>
2026-01-20 17:16:11 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -52,7 +52,7 @@ def get_session_id():
         ),
     agent_endpoint_id=agent_ep_id,
     )
-st.session_state.agent_session_id = resp.data.id
+            st.session_state.agent_session_id = resp.data.id
 
     except oci.exceptions.ServiceError as e:
         st.error(f"CreateSession failed: status={e.status}, code={e.code}, msg={e.message}")
