82c7b0aa96f611a3210ff9669cd19394213e633b
dyepth06 <jeffdelapena06@gmail.com>
2026-01-20 17:19:10 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -72,7 +72,7 @@ def response_generator(textinput: str) -> str:
 
 # Initialize chat history
     if "messages" not in st.session_state:
-        st.session_state.messages = []
+    st.session_state.messages = []
 
 # Display chat messages from history on app rerun
     for message in st.session_state.messages:
