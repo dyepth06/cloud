89ec68b8bf7a5607a1543d0f5f8cb8489673d09b
dyepth06 <jeffdelapena06@gmail.com>
2026-01-20 17:19:35 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -71,13 +71,13 @@ def response_generator(textinput: str) -> str:
     )
 
 # Initialize chat history
-    if "messages" not in st.session_state:
-    st.session_state.messages = []
+        if "messages" not in st.session_state:
+        st.session_state.messages = []
 
 # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 # Accept user input
 if prompt := st.chat_input("How can I help?"):
