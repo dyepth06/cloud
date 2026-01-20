2618520fb876497fe293d2a574cc53ef91a055f4
dyepth06 <jeffdelapena06@gmail.com>
2026-01-20 17:15:09 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -44,7 +44,7 @@ if "ga_client" not in st.session_state:
 
 def get_session_id():
     if "agent_session_id" not in st.session_state:
-    try:
+        try:
         resp = client.create_session(
         create_session_details=oci.generative_ai_agent_runtime.models.CreateSessionDetails(
         display_name="USER_Session",
