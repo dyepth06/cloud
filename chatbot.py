e8b3129bd4ff2d27b5790235cf7296d93f0f02c4
dyepth06 <jeffdelapena06@gmail.com>
2026-01-22 13:23:40 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -10,7 +10,7 @@ st.caption("Ask a question to the Oracle Gen-AI Powered Agent. Click ‘Reset ch
 #Configuration: Load credentials from Streamlit Secrets
 
 def build_oci_config() -> dict:
-    sec = st.secrets["oci"]
+    sec = st.secrets["DEFAULT"]
     cfg = {
         "user": sec["user_ocid"],
         "tenancy": sec["tenancy_ocid"],
