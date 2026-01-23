c31946905dfd9ac3a11a508b9e273d2208291900
dyepth06 <jeffdelapena06@gmail.com>
2026-01-23 15:51:27 +0800

    Uncomment exception imports in chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import oci
 from typing import Optional
-#from oci.exceptions import ServiceError, RequestException  #temproray
+from oci.exceptions import ServiceError, RequestException  #temproray
+
 
 st.set_page_config(page_title="OCI Gen-AI Chatbot", page_icon="💬", layout="centered")
 st.title("OCI Gen-AI Chatbot for Telco")
